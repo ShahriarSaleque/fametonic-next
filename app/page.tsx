aa3810a7d2cac180bc3f1a8f258bb02e1a3f16a1
shahriar saleque <shahriar.saleque@tekarsh.com>
2025-05-24 17:31:54 +0600

    add all the components, style the main content , bring in the image

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@
 
 import FametonicLogo from "@/assets/FameTonicLogo"
 import Button from "@/components/Button"
+import FeatureList from "@/components/FeatureList"
+import { featureList } from "@/lib/const"
+import Image from "next/image"
 import { useRouter } from "next/navigation"
 
 export default function Home() {
@@ -30,6 +33,67 @@ export default function Home() {
           <Button label="Contact" onClick={() => router.push("/contact")} />
         </div>
       </nav>
+
+      {/* Main Content */}
+      <div className="w-full gap-0 bg-[#010101] overflow-hidden">
+        <div className="gap-0 py-0 px-[211px] mb-3 md:mb-0 max-w-[1490px]">
+          <div className="flex flex-wrap gap-0 relative text-white">
+            <div className="w-full relative">
+              <div className="static md:absolute z-[1] right-0 top-0">
+                <Image
+                  src="/mobile.png"
+                  alt="Mobile Image"
+                  width={666}
+                  height={679}
+                  // className="w-full h-auto lg:w-[666px] lg:h-[679px] mt-10 lg:mt-0"
+                />
+              </div>
+              <div className="relative z-[2] flex flex-col justify-center min-h-0 w-full md:min-h-[679px] md:w-[45%]">
+                {/* Content section */}
+                <div className="flex flex-col items-start self-stretch lg:max-w-none max-w-[516px] flex-1 gap-[30px] mt-[57px]">
+                  <div>
+                    <div className="mb-[16px]">
+                      <div className="text-white font-urbanist text-[28px] sm:text-[35px] font-bold leading-normal self-stretch">
+                        Want to Turn Social Media Into a Profitable Career?
+                      </div>
+                      <div className="text-[#00E7F9] font-urbanist text-[28px] sm:text-[35px] font-bold leading-normal self-stretch [text-shadow:0px_4px_4px_#FC004E]">
+                        Discover your way to success with Fametonic:
+                      </div>
+                    </div>
+                    {/* Features List */}
+                    <FeatureList featureList={featureList} />
+                  </div>
+
+                  {/* CTA Section */}
+                  <div className="flex flex-col items-center">
+                    <button className="flex justify-center items-center gap-[10px] px-[40px] py-2 align-self-stretch rounded-[10px] bg-[#FC004E] shadow-[2px_2px_10px_0px_#00E7F9] w-full sm:w-[313px] h-[60px]">
+                      <div className="flex items-center gap-[10px]">
+                        <span className="text-white font-figtree text-[20px] not-italic font-bold leading-normal">
+                          GET STARTED
+                        </span>
+                      </div>
+                    </button>
+                    <p className="text-white self-stretch text-center font-figtree text-xs not-italic font-normal leading-[16px] mt-[10px]">
+                      1-minute quiz for personalized Insights
+                    </p>
+                  </div>
+
+                  {/* Footer Text */}
+                  <div className="flex flex-col gap-3">
+                    <p className="text-[#ABABAB] self-stretch font-figtree text-xs not-italic font-medium leading-normal">
+                      By clicking &quot;Get Started&quot;, you agree with Terms
+                      and Conditions, Privacy Policy, Subscription Terms
+                    </p>
+                    <p className="text-[#ABABAB] self-stretch font-figtree text-[10px] not-italic font-medium leading-normal">
+                      Fametonic 2025 ©All Rights Reserved.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
---
+++ components/FeatureList.tsx
@@ -0,0 +1,21 @@
+const FeatureList = ({ featureList }: FeatureListProps) => {
+  return (
+    <div className="flex flex-col gap-[13px]">
+      {featureList?.map((text, index) => (
+        <div key={index} className="flex items-center gap-[10px]">
+          <span className="text-white text-center font-nunito text-[22px] not-italic font-medium leading-normal">
+            ✨
+          </span>
+          <p className="text-white font-figtree text-[16px] not-italic font-semibold leading-[22px] flex-1">
+            {text}
+          </p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+type FeatureListProps = {
+  featureList: string[]
+}
+export default FeatureList
